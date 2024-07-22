18efd8978ade15bef2998e95bccbebeb2472c4cb
Flussen <53578660+Flussen@users.noreply.github.com>
2024-07-22 19:40:28 +0200

    feat: validatorController added

---
+++ src/auth-service/src/main/java/com/biznetbb/auth/web/controller/ValidatorController.java
@@ -0,0 +1,22 @@
+package com.biznetbb.auth.web.controller;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.biznetbb.auth.service.ValidateService;
+
+@RestController
+@RequestMapping("/auth")
+public class ValidatorController {
+
+    @Autowired
+    private ValidateService validator;
+
+    public ResponseEntity<?> validate(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token){
+        return this.validator.validate(token);
+    }
+}
