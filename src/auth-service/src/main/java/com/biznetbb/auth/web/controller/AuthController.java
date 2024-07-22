c5137184294f9ce0fa575f867a6e37fa693cf540
Flussen <53578660+Flussen@users.noreply.github.com>
2024-07-22 19:23:06 +0200

    feat: auth controller added

---
+++ src/auth-service/src/main/java/com/biznetbb/auth/web/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.biznetbb.auth.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.biznetbb.auth.service.AuthService;
+import com.biznetbb.auth.service.dto.LoginDto;
+import com.biznetbb.auth.service.dto.RegisterDto;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final AuthService auth;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto){
+        return auth.register(registerDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
+        return auth.login(loginDto);
+    }
+}
