b28de82c0ed7139e367e86eb0f50232c3394d8ff
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-07-23 22:19:15 +0200

    add skeletro

---
+++ src/post-manager/src/main/java/com/biznetbb/postmanager/PostManagerApplication.java
@@ -0,0 +1,13 @@
+package com.biznetbb.postmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManagerApplication.class, args);
+	}
+
+}
