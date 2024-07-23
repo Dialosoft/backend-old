d3f1e4ef0e6ba44456a3eb6f6e53fb552a55aa5b
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-07-23 22:32:14 +0200

    WIP progress in the skeletro and structure of the project

---
+++ src/post-manager/src/main/java/com/biznetbb/postmanager/services/impl/PostManagerServiceImpl.java
@@ -0,0 +1,6 @@
+package com.biznetbb.postmanager.services.impl;
+
+import com.biznetbb.postmanager.services.PostManagerService;
+
+public class PostManagerServiceImpl implements PostManagerService {
+}
