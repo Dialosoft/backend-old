8f6f3b776c4431091eb5846af037fae780d16874
matdevcoder <mat.enterprice@gmail.com>
2024-07-29 01:31:57 +0200

    refactor(auth-service): Add role and refreshToken repository, refactor user repository

--- src/auth-service/src/main/java/com/biznetbb/auth/persistence/repository/UserRepository.java
+++ src/auth-service/src/main/java/com/biznetbb/auth/persistence/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.biznetbb.auth.persistence.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.biznetbb.auth.persistence.entity.UserEntity;
+
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {
-    UserEntity findByUsername(String username);
-    UserEntity findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByUsernameOrEmail(String username, String email);
 }
