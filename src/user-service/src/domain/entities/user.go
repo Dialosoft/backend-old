78c32181665c6ae6390cbf941a2865f5011c5787
Flussen <53578660+Flussen@users.noreply.github.com>
2024-07-28 22:08:46 +0200

    feat: roles and user entities added

--- src/user-service/src/domain/entities/user.go
+++ src/user-service/src/domain/entities/user.go
@@ -1,1 +1,15 @@
 package entities
+
+import "time"
+
+type User struct {
+	UUID      string    `json:"uuid"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	RoleId    Roles     `json:"roleId"`
+	Locked    bool      `json:"locked"`
+	Disable   bool      `json:"disable"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
