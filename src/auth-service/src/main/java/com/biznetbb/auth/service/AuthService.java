b75b45f8593f71a588604ecf71312c9d53e569a9
matdevcoder <mat.enterprice@gmail.com>
2024-07-29 01:44:27 +0200

    refactor(auth-service): add refreshToken support to Auth flow logic and Generic ResponseBody

--- src/auth-service/src/main/java/com/biznetbb/auth/service/AuthService.java
+++ src/auth-service/src/main/java/com/biznetbb/auth/service/AuthService.java
@@ -1,85 +1,159 @@
 package com.biznetbb.auth.service;
 
+import com.biznetbb.auth.persistence.entity.RefreshToken;
+import com.biznetbb.auth.persistence.entity.RoleEntity;
+import com.biznetbb.auth.persistence.entity.UserEntity;
+import com.biznetbb.auth.persistence.repository.RoleRepository;
+import com.biznetbb.auth.persistence.repository.UserRepository;
+import com.biznetbb.auth.persistence.response.JwtResponseDTO;
+import com.biznetbb.auth.persistence.response.ResponseBody;
+import com.biznetbb.auth.service.dto.LoginDto;
+import com.biznetbb.auth.service.dto.RefreshTokenDto;
+import com.biznetbb.auth.service.dto.RegisterDto;
+import com.biznetbb.auth.service.utils.RoleType;
+import com.biznetbb.auth.web.config.SecurityConfig;
+import com.biznetbb.auth.web.config.jwt.JwtUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.biznetbb.auth.persistence.entity.UserEntity;
-import com.biznetbb.auth.persistence.repository.UserRepository;
-import com.biznetbb.auth.persistence.response.ResponseBody;
-import com.biznetbb.auth.service.dto.LoginDto;
-import com.biznetbb.auth.service.dto.RegisterDto;
-import com.biznetbb.auth.web.config.SecurityConfig;
-import com.biznetbb.auth.web.config.jwt.JwtUtil;
-
-import lombok.AllArgsConstructor;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
-    
+
     private final UserRepository userRepository;
-    private final SecurityConfig config;
+    private final RoleRepository roleRepository;
+    private final UserSecurityService userSecurityService;
+    private final RefreshTokenService refreshTokenService;
+    private final SecurityConfig securityConfig;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
-    
-    public ResponseEntity<ResponseBody> register(RegisterDto registerDto){
-        ResponseBody response = new ResponseBody();
 
-        if(userRepository.findByUsername(registerDto.getUsername()) != null &&
-            userRepository.findByEmail(registerDto.getEmail()) != null){
+    public ResponseEntity<ResponseBody<?>> register(RegisterDto registerDto) {
 
-            response.setStatusCode(HttpStatus.CONFLICT.value());
-            response.setMessage("any of the parameters already exists");
-            response.setMetadata(null);
+        Optional<UserEntity> userEntityOp = userRepository.findByUsernameOrEmail(registerDto.getUsername(), registerDto.getEmail());
+
+        if (userEntityOp.isPresent()) {
+
+            ResponseBody<?> response = ResponseBody.builder()
+                    .statusCode(HttpStatus.CONFLICT.value())
+                    .message("any of the parameters already exists")
+                    .metadata(null)
+                    .build();
+
             return new ResponseEntity<>(response, HttpStatus.CONFLICT);
         }
 
-        UserEntity userEntity = new UserEntity();
-        userEntity.setUsername(registerDto.getUsername());
-        userEntity.setEmail(registerDto.getEmail());
-        userEntity.setPassword(config.encoder().encode(registerDto.getPassword()));
-        UserEntity newUser = userRepository.save(userEntity);
+        UserEntity newUser;
 
-        if(newUser == null){
-            response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            response.setMessage("user could not be created");
-            response.setMetadata(null);
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            RoleEntity defaultRole = roleRepository.findByRoleType(RoleType.USER)
+                    .orElseThrow(() -> new RuntimeException("Default role not found"));
+
+            UserEntity userEntity = UserEntity.builder()
+                    .username(registerDto.getUsername())
+                    .email(registerDto.getEmail())
+                    .password(securityConfig.encoder().encode(registerDto.getPassword()))
+                    .roles(Collections.singleton(defaultRole))
+                    .build();
+
+            newUser = userRepository.save(userEntity);
+        } catch (Exception e) {
+            ResponseBody response = ResponseBody.builder()
+                    .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .message("An error occurred while creating the user")
+                    .metadata(null)
+                    .build();
+
+            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
         }
 
-        response.setStatusCode(HttpStatus.CREATED.value());
-        response.setMessage("User " + newUser.getUuid() + " created sucessfully");
-        response.setMetadata(null);
+        ResponseBody<?> response = ResponseBody.builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message(String.format("User %s created sucessfully", newUser.getId()))
+                .metadata(null)
+                .build();
+
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    public ResponseEntity<ResponseBody> login(LoginDto loginDto){
-        ResponseBody response = new ResponseBody();
+    public ResponseEntity<ResponseBody<?>> login(LoginDto loginDto) {
+
         UsernamePasswordAuthenticationToken login = new UsernamePasswordAuthenticationToken(
-            loginDto.getUsername(),
-            loginDto.getPassword()
-            );
+                loginDto.getUsername(),
+                loginDto.getPassword()
+        );
+
         try {
             Authentication authentication = this.authenticationManager.authenticate(login);
-            System.out.println(authentication.getPrincipal());
-            String jwt = jwtUtil.create(loginDto.getUsername());
 
-            response.setStatusCode(HttpStatus.OK.value());
-            response.setMessage("authenticated successfully");
-            response.setMetadata(jwt);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-            
-            
+            if(authentication.isAuthenticated()) {
+
+                String accessToken = jwtUtil.generateAccessToken(loginDto.getUsername(), authentication.getAuthorities());
+                RefreshToken refreshToken = refreshTokenService.createRefreshToken(loginDto.getUsername());
+
+                JwtResponseDTO jwtResponseDTO = JwtResponseDTO.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken.getRefreshToken())
+                        .build();
+
+                ResponseBody<JwtResponseDTO> response = ResponseBody.<JwtResponseDTO>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Authentication successfully")
+                        .metadata(jwtResponseDTO)
+                        .build();
+
+                return new ResponseEntity<>(response, HttpStatus.OK);
+
+            } else {
+                throw new UsernameNotFoundException("invalid user request..!!");
+            }
+
         } catch (BadCredentialsException e) {
-            response.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-            response.setMessage("Unauthorized");
-            response.setMetadata(null);
+
+            ResponseBody<?> response = ResponseBody.builder()
+                    .statusCode(HttpStatus.UNAUTHORIZED.value())
+                    .message("Unauthorized")
+                    .metadata(null)
+                    .build();
+
             return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    public ResponseEntity<ResponseBody<JwtResponseDTO>> refreshTokens(RefreshTokenDto refreshTokenDto) {
+
+        return refreshTokenService.findByToken(refreshTokenDto.getRefreshToken())
+                .map(refreshTokenService::verifyRefreshTokenExpiration)
+                .map(RefreshToken::getUser)
+                .map(userInfo -> {
+
+                    String username = userInfo.getUsername();
+                    UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+                    String accessToken = jwtUtil.generateAccessToken(username,userDetails.getAuthorities());
+
+                    JwtResponseDTO jwtResponseDTO = JwtResponseDTO.builder()
+                            .accessToken(accessToken)
+                            .refreshToken(refreshTokenDto.getRefreshToken()).build();
+
+                    ResponseBody<JwtResponseDTO> response = ResponseBody.<JwtResponseDTO>builder()
+                            .statusCode(HttpStatus.OK.value())
+                            .message("Pair tokens created successfully")
+                            .metadata(jwtResponseDTO)
+                            .build();
+
+                    return new ResponseEntity<>(response, HttpStatus.OK);
+                })
+                .orElseThrow(() -> new RuntimeException(String.format("Refresh token: '%s' was not found in our system", refreshTokenDto.getRefreshToken())));
     }
 }
--- src/auth-service/src/main/java/com/biznetbb/auth/web/controller/AuthController.java
+++ src/auth-service/src/main/java/com/biznetbb/auth/web/controller/AuthController.java
@@ -1,31 +1,41 @@
 package com.biznetbb.auth.web.controller;
 
+import com.biznetbb.auth.persistence.response.JwtResponseDTO;
+import com.biznetbb.auth.persistence.response.ResponseBody;
+import com.biznetbb.auth.service.AuthService;
+import com.biznetbb.auth.service.RefreshTokenService;
+import com.biznetbb.auth.service.dto.LoginDto;
+import com.biznetbb.auth.service.dto.RefreshTokenDto;
+import com.biznetbb.auth.service.dto.RegisterDto;
+import com.biznetbb.auth.web.config.jwt.JwtUtil;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.biznetbb.auth.persistence.response.ResponseBody;
-import com.biznetbb.auth.service.AuthService;
-import com.biznetbb.auth.service.dto.LoginDto;
-import com.biznetbb.auth.service.dto.RegisterDto;
-
-import lombok.AllArgsConstructor;
-
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-    private final AuthService auth;
+    private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseBody> register(@RequestBody RegisterDto registerDto){
-        return auth.register(registerDto);
+    public ResponseEntity<ResponseBody<?>> register(@RequestBody RegisterDto registerDto) {
+        return authService.register(registerDto);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
-        return auth.login(loginDto);
+    public ResponseEntity<ResponseBody<?>> login(@RequestBody LoginDto loginDto) {
+        return authService.login(loginDto);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<ResponseBody<JwtResponseDTO>> refreshtoken(@Valid @RequestBody RefreshTokenDto request) {
+        return authService.refreshTokens(request);
     }
 }
