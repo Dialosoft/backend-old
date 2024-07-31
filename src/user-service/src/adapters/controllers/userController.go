ea00f81971f81d669854aa1c9e1716644cc499ce
Flussen <53578660+Flussen@users.noreply.github.com>
2024-07-31 20:21:19 +0200

    feat: userController added

--- src/user-service/src/adapters/controllers/userController.go
+++ src/user-service/src/adapters/controllers/userController.go
@@ -8,4 +8,17 @@ import (
 
 func Home(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "home!"})
+
+}
+
+func ChangeEmailController(c *gin.Context) {
+
+}
+
+func ChangeUserAvatarController(c *gin.Context) {
+
+}
+
+func ChangePasswordController(c *gin.Context) {
+
 }
