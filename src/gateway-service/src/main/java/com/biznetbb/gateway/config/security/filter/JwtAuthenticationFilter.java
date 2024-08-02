37b29dae3ded6e2fc6ba796c20acfeab538d0924
matdevcoder <mat.enterprice@gmail.com>
2024-08-02 19:43:58 +0200

    refactor(gateway-service): Add authentication filter and utils

---
+++ src/gateway-service/src/main/java/com/biznetbb/gateway/config/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.biznetbb.gateway.config.security.filter;
+
+import com.biznetbb.gateway.config.security.util.JwtUtils;
+import com.biznetbb.gateway.config.security.util.AuthUtils;
+import com.biznetbb.gateway.config.security.util.RouterValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private AuthUtils authUtils;
+    @Autowired
+    private RouterValidator routerValidator;
+
+    public JwtAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+        // Configuration properties if needed
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            var request = exchange.getRequest();
+
+            if (routerValidator.isSecured(request)) {
+
+                if (authUtils.isAuthorizationHeaderMissing(request)) {
+                    return authUtils.onError(exchange, HttpStatus.UNAUTHORIZED);
+                }
+
+                String token = authUtils.getAuthorizationHeader(request);
+
+                if (token == null || !token.startsWith("Bearer ")) {
+                    return authUtils.onError(exchange, HttpStatus.FORBIDDEN, "Missing or invalid Authorization header");
+                }
+
+                token = token.substring(7);
+
+                if (!jwtUtils.isValid(token)) {
+                    return authUtils.onError(exchange, HttpStatus.FORBIDDEN);
+                }
+
+                authUtils.mutateRequestWithAuthHeaders(exchange, token);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+
+}
