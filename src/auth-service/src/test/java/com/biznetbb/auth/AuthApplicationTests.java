855b2d42a95be227e72b008f32339f8d1b598ab6
matdevcoder <mat.enterprice@gmail.com>
2024-08-04 00:56:18 +0200

    refactir: move docker-compose.yml to root and do little changes over env variables

--- src/auth-service/src/test/java/com/biznetbb/auth/AuthApplicationTests.java
+++ src/auth-service/src/test/java/com/biznetbb/auth/AuthApplicationTests.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class AuthApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class AuthApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
