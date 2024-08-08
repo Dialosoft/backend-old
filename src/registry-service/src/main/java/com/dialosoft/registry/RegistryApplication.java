9d95160414792d8f4ccf329886c0df7e9ee6d6b8
matdevcoder <mat.enterprice@gmail.com>
2024-08-08 19:50:53 +0200

    refactor(registry-service): rebranding to dialosoft

--- src/registry-service/src/main/java/com/dialosoft/registry/RegistryApplication.java
+++ src/registry-service/src/main/java/com/dialosoft/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.biznetbb.registry;
+package com.dialosoft.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/registry-service/src/test/java/com/dialosoft/registry/RegistryApplicationTests.java
+++ src/registry-service/src/test/java/com/dialosoft/registry/RegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.biznetbb.registry;
+package com.dialosoft.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
