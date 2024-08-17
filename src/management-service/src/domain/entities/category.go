9970b4d4f01f1ff0360bf6beab144eb65c3422bc
Flussen <53578660+Flussen@users.noreply.github.com>
2024-08-17 14:36:33 +0200

    refactor: refactorized the forum and category models

--- src/management-service/src/domain/entities/category.go
+++ src/management-service/src/domain/entities/category.go
@@ -1,8 +1,17 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Category struct {
-	ID   uuid.UUID `gorm:"type:uuid;primary_key" json:"uuid"`
-	Name string    `gorm:"type:varchar(30);unique;not null" json:"name"`
+	ID          uuid.UUID      `gorm:"type:uuid;primary_key" json:"id"`
+	Name        string         `gorm:"type:varchar(30);unique;not null" json:"name"`
+	Description string         `gorm:"type:varchar(255)" json:"description"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
--- src/management-service/src/domain/entities/forum.go
+++ src/management-service/src/domain/entities/forum.go
@@ -1,10 +1,21 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Forum struct {
-	ID         uuid.UUID `gorm:"type:uuid;primary_key" json:"uuid"`
-	Name       string    `gorm:"type:varchar(30);unique;not null" json:"name"`
-	CategoryID string    `gorm:"not null" json:"categoryId"`
-	Category   Category  `gorm:"foreignKey:CategoryID;references:ID" json:"Category"`
+	ID          uuid.UUID      `gorm:"type:uuid;primary_key" json:"id"`
+	Name        string         `gorm:"type:varchar(30);unique;not null" json:"name"`
+	Description string         `gorm:"type:varchar(255)" json:"description"`
+	IsActive    bool           `gorm:"default:true" json:"is_active"`
+	PostCount   uint32         `gorm:"default:0" json:"post_count"`
+	CategoryID  string         `gorm:"not null" json:"category_id"`
+	Category    Category       `gorm:"foreignKey:CategoryID;references:ID" json:"category"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeleteAt    gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
