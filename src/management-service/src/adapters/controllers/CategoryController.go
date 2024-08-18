cf408b46339b19fc02f76cc6c722ef0ca56f9efb
Flussen <53578660+Flussen@users.noreply.github.com>
2024-08-18 14:02:15 +0200

    refactor: implementation fo the erroResponses functions

--- src/management-service/src/adapters/controllers/CategoryController.go
+++ src/management-service/src/adapters/controllers/CategoryController.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Dialosoft/management-service/src/application/services"
 	"github.com/Dialosoft/management-service/src/domain/entities/request"
 	"github.com/Dialosoft/management-service/src/domain/entities/response"
-	utility "github.com/Dialosoft/management-service/src/utils"
+	"github.com/Dialosoft/management-service/src/utility"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -38,20 +38,17 @@ func GetCategoryByID(c *gin.Context, service services.CategoryService) {
 
 	uuid, err := uuid.Parse(id)
 	if err != nil {
-		res.StatusCode = http.StatusBadRequest
-		res.Message = "UUID INVALID"
-		res.Data = nil
-		c.JSON(http.StatusBadRequest, res)
+		utility.ErrInvalidUUID(c)
 		return
 	}
 
 	categories, err := service.GetCategoryByID(uuid)
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
-			res.StatusCode = http.StatusNotFound
-			res.Message = "NOT FOUND"
-			res.Data = nil
-			c.JSON(http.StatusNotFound, res)
+			utility.ErrNotFound(c)
+			return
+		} else {
+			utility.ErrInternalServer(c)
 			return
 		}
 	}
@@ -67,25 +64,16 @@ func CreateCategory(c *gin.Context, service services.CategoryService) {
 	var req request.CreateCategoryRequest
 
 	if err := c.BindJSON(&req); err != nil {
-		res.StatusCode = http.StatusBadRequest
-		res.Message = "BAD REQUEST"
-		res.Data = nil
-		c.JSON(http.StatusBadRequest, res)
+		utility.ErrBadRequest(c)
 		return
 	}
 
 	if err := service.CreateCategory(req.Name, req.Description); err != nil {
 		if err == gorm.ErrDuplicatedKey {
-			res.StatusCode = http.StatusConflict
-			res.Message = "CONFLICT"
-			res.Data = nil
-			c.JSON(http.StatusConflict, res)
+			utility.ErrConflict(c)
 			return
 		} else {
-			res.StatusCode = http.StatusInternalServerError
-			res.Message = "INTERNAL SERVER ERROR"
-			res.Data = nil
-			c.JSON(http.StatusInternalServerError, res)
+			utility.ErrInternalServer(c)
 			return
 		}
 	}
@@ -103,10 +91,7 @@ func UpdateCategory(c *gin.Context, service services.CategoryService) {
 
 	uuid, err := uuid.Parse(id)
 	if err != nil {
-		res.StatusCode = http.StatusBadRequest
-		res.Message = "UUID INVALID"
-		res.Data = nil
-		c.JSON(http.StatusBadRequest, res)
+		utility.ErrInvalidUUID(c)
 		return
 	}
 
@@ -120,10 +105,7 @@ func UpdateCategory(c *gin.Context, service services.CategoryService) {
 
 	if err := service.UpdateCategory(uuid, req.Name, req.Description); err != nil {
 		if err == gorm.ErrRecordNotFound {
-			res.StatusCode = http.StatusNotFound
-			res.Message = "NOT FOUND"
-			res.Data = nil
-			c.JSON(http.StatusNotFound, res)
+			utility.ErrNotFound(c)
 			return
 		} else {
 			res.StatusCode = http.StatusInternalServerError
@@ -145,25 +127,16 @@ func DeleteCategory(c *gin.Context, service services.CategoryService) {
 
 	uuid, err := uuid.Parse(id)
 	if err != nil {
-		res.StatusCode = http.StatusBadRequest
-		res.Message = "UUID INVALID"
-		res.Data = nil
-		c.JSON(http.StatusBadRequest, res)
+		utility.ErrInvalidUUID(c)
 		return
 	}
 
 	if err := service.DeleteCategory(uuid); err != nil {
 		if err == gorm.ErrRecordNotFound {
-			res.StatusCode = http.StatusNotFound
-			res.Message = "NOT FOUND"
-			res.Data = nil
-			c.JSON(http.StatusNotFound, res)
+			utility.ErrNotFound(c)
 			return
 		} else {
-			res.StatusCode = http.StatusInternalServerError
-			res.Message = "INTERNAL SERVER ERROR"
-			res.Data = nil
-			c.JSON(http.StatusInternalServerError, res)
+			utility.ErrInternalServer(c)
 			return
 		}
 	}
@@ -180,25 +153,16 @@ func RestoreCategory(c *gin.Context, service services.CategoryService) {
 
 	uuid, err := uuid.Parse(id)
 	if err != nil {
-		res.StatusCode = http.StatusBadRequest
-		res.Message = "UUID INVALID"
-		res.Data = nil
-		c.JSON(http.StatusBadRequest, res)
+		utility.ErrInvalidUUID(c)
 		return
 	}
 
 	if err := service.RestoreCategory(uuid); err != nil {
 		if err == gorm.ErrRecordNotFound {
-			res.StatusCode = http.StatusNotFound
-			res.Message = "NOT FOUND"
-			res.Data = nil
-			c.JSON(http.StatusNotFound, res)
+			utility.ErrNotFound(c)
 			return
 		} else {
-			res.StatusCode = http.StatusInternalServerError
-			res.Message = "INTERNAL SERVER ERROR"
-			res.Data = nil
-			c.JSON(http.StatusInternalServerError, res)
+			utility.ErrInternalServer(c)
 			return
 		}
 	}
