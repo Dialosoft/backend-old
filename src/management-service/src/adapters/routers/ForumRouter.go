b90d4be22d79ee7f0ea0b88ea4e5f7dd263a9f0f
Flussen <53578660+Flussen@users.noreply.github.com>
2024-08-18 19:16:40 +0200

    feat: forum router addeds

--- src/management-service/src/adapters/routers/ForumRouter.go
+++ src/management-service/src/adapters/routers/ForumRouter.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"github.com/Dialosoft/management-service/src/adapters/controllers"
 	"github.com/Dialosoft/management-service/src/application/services"
 	"github.com/gin-gonic/gin"
 )
@@ -16,10 +17,39 @@ func NewForumRouter(service services.ForumService) *ForumRouter {
 func (r *ForumRouter) SetupForumRoutes(group *gin.RouterGroup) {
 	forumGroup := group.Group("/forums")
 	{
-		forumGroup.GET("/test", r.Test)
+		forumGroup.GET("/get-all-forums", r.getAllForumsHandle)
+		forumGroup.GET("/get-forum-id/:id", r.getForumByIdHandle)
+		forumGroup.POST("/create-forum", r.createForumHandle)
+		forumGroup.PUT("/update-forum-category", r.updateForumCategoryOwnerHandle)
+		forumGroup.PUT("/update-forum-name", r.updateForumNameHandle)
+		forumGroup.PUT("/update-forum-description", r.updateForumDescriptionHandle)
+		forumGroup.DELETE("/delete-forum/:id", r.deleteForumHandleHandle)
+		forumGroup.POST("/restore-forum/:id", r.restoreForumHandle)
 	}
 }
 
-func (r *ForumRouter) Test(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "ok"})
+func (r *ForumRouter) getAllForumsHandle(c *gin.Context) {
+	controllers.GetAllForums(c, r.ForumService)
+}
+
+func (r *ForumRouter) getForumByIdHandle(c *gin.Context) {
+	controllers.GetForumByID(c, r.ForumService)
+}
+func (r *ForumRouter) createForumHandle(c *gin.Context) {
+	controllers.CreateForum(c, r.ForumService)
+}
+func (r *ForumRouter) updateForumCategoryOwnerHandle(c *gin.Context) {
+	controllers.UpdateForumCategoryOwner(c, r.ForumService)
+}
+func (r *ForumRouter) updateForumNameHandle(c *gin.Context) {
+	controllers.UpdateForumName(c, r.ForumService)
+}
+func (r *ForumRouter) updateForumDescriptionHandle(c *gin.Context) {
+	controllers.UpdateForumDescription(c, r.ForumService)
+}
+func (r *ForumRouter) deleteForumHandleHandle(c *gin.Context) {
+	controllers.DeleteForum(c, r.ForumService)
+}
+func (r *ForumRouter) restoreForumHandle(c *gin.Context) {
+	controllers.RestoreForum(c, r.ForumService)
 }
