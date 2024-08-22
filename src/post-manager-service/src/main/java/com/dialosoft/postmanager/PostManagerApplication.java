19fe853e15321c193192c5ad978e21d69537b8c2
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-08-22 11:04:39 +0200

    feat: enable discovery client

--- src/post-manager-service/src/main/java/com/dialosoft/postmanager/PostManagerApplication.java
+++ src/post-manager-service/src/main/java/com/dialosoft/postmanager/PostManagerApplication.java
@@ -3,12 +3,12 @@ package com.dialosoft.postmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableJpaRepositories
 public class PostManagerApplication {
 
