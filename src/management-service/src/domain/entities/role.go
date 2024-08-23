ca5619bb5c91c8c7dffd13749ae4e3367d0b54d8
Flussen <53578660+Flussen@users.noreply.github.com>
2024-08-23 14:54:23 +0200

    feat: role entity uuid.UUID type changes

--- src/management-service/src/domain/entities/role.go
+++ src/management-service/src/domain/entities/role.go
@@ -1,9 +1,11 @@
 package entities
 
+import "github.com/google/uuid"
+
 type Role struct {
-	ID         string `gorm:"primary_key" json:"id"`
-	AdminRole  bool   `gorm:"default:false" json:"admin_role"`
-	ModRole    bool   `gorm:"default:false" json:"mod_role"`
-	Permission int    `json:"permission"`
-	RoleType   string `gorm:"type:varchar(50)" json:"role_type"`
+	ID         uuid.UUID `gorm:"primary_key" json:"id"`
+	AdminRole  bool      `gorm:"default:false" json:"admin_role"`
+	ModRole    bool      `gorm:"default:false" json:"mod_role"`
+	Permission int       `json:"permission"`
+	RoleType   string    `gorm:"type:varchar(50)" json:"role_type"`
 }
